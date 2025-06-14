a3216495eebe5b8eaabe2d68371f6b3bc8682013
Jeidy Olaya <jeidkata@gmail.com>
2025-06-13 20:10:37 -0500

    Creo test para crear un usuario en la api. Paso el test.

--- src/test/java/com/globant/swaggerPetstoreAPI/test/UserTest.java
+++ src/test/java/com/globant/swaggerPetstoreAPI/test/UserTest.java
@@ -1,7 +1,35 @@
 package com.globant.swaggerPetstoreAPI.test;
 
 import com.globant.swaggerPetstoreAPI.config.TestRunner;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.apache.http.entity.ContentType;
+import org.testng.annotations.Test;
 
 public class UserTest extends TestRunner {
-
+    @Test(testName = "Create a new user")
+    public void createUserTest() {
+        RestAssured
+                .given()
+                .baseUri(getApiUrl())
+                .header("content-type", ContentType.APPLICATION_JSON.getMimeType())
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
+                .body("{\n" +
+                        "  \"id\": 1,\n" +
+                        "  \"username\": \"Jeidy\",\n" +
+                        "  \"firstName\": \"jeidy\",\n" +
+                        "  \"lastName\": \"olaya\",\n" +
+                        "  \"email\": \"jkos@gmail.com\",\n" +
+                        "  \"password\": \"J1234y\",\n" +
+                        "  \"phone\": \"12345\",\n" +
+                        "  \"userStatus\": 0\n" +
+                        "}")
+                .when()
+                .post("/user")
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
 }
