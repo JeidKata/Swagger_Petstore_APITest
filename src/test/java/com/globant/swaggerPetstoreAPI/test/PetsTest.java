d8916092c99463750e7304ad885746d72ebf4468
Jeidy Olaya <jeidkata@gmail.com>
2025-06-13 21:17:25 -0500

    Creo test para el listar las mascotas con estado 'disponible' en la api. Paso el test.

--- src/test/java/com/globant/swaggerPetstoreAPI/test/PetsTest.java
+++ src/test/java/com/globant/swaggerPetstoreAPI/test/PetsTest.java
@@ -1,6 +1,35 @@
 package com.globant.swaggerPetstoreAPI.test;
 
 import com.globant.swaggerPetstoreAPI.config.TestRunner;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.response.Response;
+import org.apache.http.entity.ContentType;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class PetsTest extends TestRunner {
+    private long specificPetId;
+
+    @Test(testName = "Get all pets available",
+          description = "This test retrieves all pets from the API and verifies the response.",
+          groups = {"pets"})
+    public List<Integer> getAvailablePetsTest() {
+        Response response = RestAssured
+                .given()
+                .baseUri(getApiUrl())
+                .header("content-type", ContentType.APPLICATION_JSON.getMimeType())
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
+                .when()
+                .get("/pet/findByStatus?status=available")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        List<Integer> petIds = response.jsonPath().getList("id");
+        return petIds;
+    }
 }
