22865ec12d4ceaac7850010a4396ba131aff5ebe
Jeidy Olaya <jeidkata@gmail.com>
2025-06-13 22:00:15 -0500

    Creo requestBuilder para refactorizar el código.

---
+++ src/test/java/com/globant/swaggerPetstoreAPI/request/RequestBuilder.java
@@ -0,0 +1,26 @@
+package com.globant.swaggerPetstoreAPI.request;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpHeaders;
+import org.apache.http.entity.ContentType;
+
+
+public class RequestBuilder {
+    private static final String APIKEY = "apiKey";
+
+    public static Response getRequest(String apiUrl, String path, String apiKey ) {
+        RequestSpecification requestSpecification = RestAssured.given()
+                .baseUri(apiUrl)
+                .header(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.getMimeType())
+                .header(APIKEY, apiKey)
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter());
+
+
+        return requestSpecification.get(path);
+    }
+}
