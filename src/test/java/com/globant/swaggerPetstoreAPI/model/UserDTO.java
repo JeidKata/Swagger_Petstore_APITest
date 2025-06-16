67992569de221a71cc087f5c7f1dcbcf582053f5
Jeidy Olaya <jeidkata@gmail.com>
2025-06-16 07:50:48 -0500

    Creo modelos para poder mapear los objetos. Por eso UserDTO tiene los atributos correspondientes para la creación de un usuraio. Uso las anotaciones de lombok y se hace para descerializar los datos para mapear la información.

---
+++ src/test/java/com/globant/swaggerPetstoreAPI/model/UserDTO.java
@@ -0,0 +1,19 @@
+package com.globant.swaggerPetstoreAPI.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Lombok annotations to generate getters, setters, toString, equals, and hashCode methods
+@NoArgsConstructor // Default constructor
+@AllArgsConstructor // Constructor with all fields
+public class UserDTO {
+    private String id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private int userStatus;
+}
