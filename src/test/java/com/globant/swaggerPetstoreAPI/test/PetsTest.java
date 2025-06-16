69c9f6938512fbcacda0728bc07d0a6d0ace856f
Jeidy Olaya <jeidkata@gmail.com>
2025-06-16 09:04:59 -0500

    Refactorizo el método getAvailablePetsTest, cambio en tipo de salida del método y creo la variable donde tendra la lista de IDs.

--- src/test/java/com/globant/swaggerPetstoreAPI/test/PetsTest.java
+++ src/test/java/com/globant/swaggerPetstoreAPI/test/PetsTest.java
@@ -1,35 +1,22 @@
 package com.globant.swaggerPetstoreAPI.test;
 
 import com.globant.swaggerPetstoreAPI.config.TestRunner;
-import io.restassured.RestAssured;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
+import com.globant.swaggerPetstoreAPI.request.RequestBuilder;
 import io.restassured.response.Response;
-import org.apache.http.entity.ContentType;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class PetsTest extends TestRunner {
+
+    private List<Integer> petId;
     private long specificPetId;
 
-    @Test(testName = "Get all pets available",
-          description = "This test retrieves all pets from the API and verifies the response.",
-          groups = {"pets"})
-    public List<Integer> getAvailablePetsTest() {
-        Response response = RestAssured
-                .given()
-                .baseUri(getApiUrl())
-                .header("content-type", ContentType.APPLICATION_JSON.getMimeType())
-                .filter(new RequestLoggingFilter())
-                .filter(new ResponseLoggingFilter())
-                .when()
-                .get("/pet/findByStatus?status=available")
-                .then()
-                .statusCode(200)
-                .extract().response();
+    @Test(testName = "Get all pets available")
+    public void getAvailablePetsTest() {
+        Response response = RequestBuilder.getRequest(getApiUrl(), "/pet/findByStatus?status=available", getApiKey());
 
         List<Integer> petIds = response.jsonPath().getList("id");
-        return petIds;
+        petId = petIds;
     }
 }
