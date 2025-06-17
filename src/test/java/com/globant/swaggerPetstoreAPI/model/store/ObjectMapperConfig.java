1650fd207019f4a509265bd8a9a4134a8e984895
Jeidy Olaya <jeidkata@gmail.com>
2025-06-16 21:58:26 -0500

    Agrego comentarios.

--- src/test/java/com/globant/swaggerPetstoreAPI/model/store/ObjectMapperConfig.java
+++ src/test/java/com/globant/swaggerPetstoreAPI/model/store/ObjectMapperConfig.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ObjectMapperConfig {
+    /**
+     * Creates and configures an ObjectMapper instance with the JavaTimeModule registered.
+     */
     public static ObjectMapper getObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
